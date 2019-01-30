5648f8c24a1b41bc6a6d6045481ad7f5d33c05cd
Reed Taylor <rtaylor@umail.ucsb.edu>
2019-01-29 19:53:41 -0800

    Added log in fucntionality

--- client.py
+++ client.py
@@ -2,12 +2,23 @@ from socket import *
 from Crypto.Cipher import AES
 from Crypto import Random
 from Crypto.Util import Counter
+from sys import exit
+import signal, os
+import pickle
+
+def signal_handler(signum, frame):
+    print "Closing connection"
+    clientSocket.send("11")
+    clientSocket.close()
+    exit()
 
 serverName = 'localhost'
 serverPort = 12000
 
 clientSocket = socket(AF_INET, SOCK_STREAM)
 clientSocket.connect((serverName, serverPort))
+
+signal.signal(signal.SIGINT, signal_handler)
 
 key = Random.new().read(16)
 iv = Random.new().read(16)
@@ -16,15 +27,43 @@ encryptAES = AES.new(key, AES.MODE_CTR, counter=encryptCtr)
 decryptCTR = Counter.new(128, initial_value=long(iv.encode("hex"), 16))
 decryptAES = AES.new(key, AES.MODE_CTR, counter=decryptCTR)
 
+while (1): #setup
+    setupMessage = clientSocket.recv(1024)
+
+    if setupMessage[0:2] == "00":
+        username = raw_input('Enter Username: ')
+        password = raw_input('Enter Password: ')
+        clientSocket.send(username)
+        clientSocket.send(password)
+
+    elif setupMessage[0:2] == "01":
+        print("Welcome " + username + "!")
+        print("Users connected:")
+        for x in pickle.loads(setupMessage[2:]):
+            print(x)
+        clientSocket.send("03" + raw_input('Select user to talk to: '))
+
+    elif setupMessage[0:2] == "02":
+        print("Incorrect Username/Password or already logged in")
+        clientSocket.close()
+        exit()
+
+    elif setupMessage[0:2] == "04":
+        break #TEMP
+
+    else:
+        print "Error: Failed setup"
+        clientSocket.close()
+        exit()
 
 while True:
     sentence = raw_input('Enter message: ')
 
     ciphertext = encryptAES.encrypt(sentence)
-    clientSocket.send(ciphertext)
+    clientSocket.send("10" + ciphertext)
 
     serverMessage = clientSocket.recv(1024)
-    plaintext = decryptAES.decrypt(serverMessage)
+    plaintext = decryptAES.decrypt(serverMessage[2:])
     print 'From Server: ', plaintext
 
 clientSocket.close()
--- server.py
+++ server.py
@@ -1,18 +1,60 @@
+###
+#Alerts: 00=enter user/pass, 01 successful login, 02, failed log in, 11 close connection, 03 select user, 04 successful select, 05 failed select
+#Message Format: "10message" where message is the text to send
+###
 from socket import *
 from Crypto.Cipher import AES
 from Crypto import Random
 import thread
+import pickle
 
-# this function is ran within a new thread whenever a new client connects.
+activeUsers = []
+
+#check user name and password for logging in
+def checkuser(username, password):
+    global activeUsers
+    f = open("users.txt", "r")
+    for x in f:
+        user = x.split("#") #separate username from password
+        if (user[0] == username and user[1].strip("\n") == password and user[0] not in activeUsers):
+            activeUsers.append(username)
+            return(True)
+    return(False)
+
+# this function is run within a new thread whenever a new client connects.
 def clientHandler(clientConnectionSocket, addr):
+    global activeUsers
+    clientConnectionSocket.send("00") #alert client to enter username and password
+    username = clientConnectionSocket.recv(1024)
+    password = clientConnectionSocket.recv(1024)
+
+    if (checkuser(username, password) == False):
+        clientConnectionSocket.send("02") #alert failed log in
+        clientConnectionSocket.close()
+        return
+    else:
+        clientConnectionSocket.send("01" + pickle.dumps(activeUsers))
 
     while True:
-
         clientMessage = clientConnectionSocket.recv(1024)
-        clientConnectionSocket.send(clientMessage)
+
+        if clientMessage[0:2] == "03": #select user
+            friend = clientMessage[2:]
+            match = friend in activeUsers #find active user not complete
+            if (match):
+                clientConnectionSocket.send("04")
+            else:
+                clientConnectionSocket.send("01" + pickle.dumps(activeUsers))
+
+        if clientMessage[0:2] == "11": #code to close
+            break
+
+        if clientMessage[0:2] == "10":
+            clientConnectionSocket.send(clientMessage)
 
     clientConnectionSocket.close()
-
+    activeUsers.remove(username)
+    return
 
 serverPort = 12000
 
@@ -24,6 +66,7 @@ print('The server is ready to receive')
 
 while True:
     connectionSocket, addr = serverSocket.accept()
+    print("client connected @", addr, " ", connectionSocket)
 
     thread.start_new_thread(clientHandler, (connectionSocket, addr))
 
